395905ef7d28bb239ad89413cd897cd67f96f624
asus test <msa-99@outlook.com>
2024-08-26 15:02:10 +0600

    BST explain

---
+++ BST_intro.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Shudhu matro sorted binary tree te search kora. Binary Tree + Binary Search
+Sorted Binary Tree te kuno ektu node er left subtree te oi node er caite chuto value thakbe and right subtree te boro value thakbe.
+*/
+int main(){
+    
+    return 0;
+}

59105a85ffdec3d27c75d17511205cbdef325408
asus test <msa-99@outlook.com>
2024-08-28 16:24:22 +0600

    insert in max Heap

---
+++ insertInMaxHeap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={50,40,45,30,35,42,32,25,20,10};
+    int x;
+    cin>>x;
+    v.push_back(x);
+    int currIndex=v.size()-1;
+    //vector er last index elem er sathe tar parent element er compare korte hobe.
+    return 0;
+}

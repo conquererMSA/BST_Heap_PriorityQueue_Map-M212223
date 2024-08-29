fd57c42da0c98cac65cc86f50419be6d70bfee3d
asus test <msa-99@outlook.com>
2024-08-29 16:04:15 +0600

    set function iterator

--- set.cpp
+++ set.cpp
@@ -5,6 +5,16 @@ using namespace std;
  Set sorted value dey, set duplicate value rakhe na. Set logN complexity te value return kore.
 */
 int main(){
-
+    set<int> s;
+    s.insert(1);
+    s.insert(100);
+    s.insert(20);
+    cout<<s.count(1000)<<endl; //0
+    cout<<s.count(100)<<endl; //1
+    //reverse print set values
+    for(auto it=s.rbegin(); it!=s.rend(); it++){
+        //Set, ascending order e sort kore
+        cout<<*it<<" ";
+    }
     return 0;
 }
